b59e32d341e2a304a975d10bfbb0684b06a7687d
Khaled Kessoum <ckalandk.main@gmail.com>
2022-08-17 15:49:18 +0100

    add benchmark tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-#include <memoize.hpp>
+#include "../src/memoize.hpp"
+#include <benchmark/benchmark.h>
 
 size_t fibo(size_t n)
 {
-    if (n <= 1) return n;
-    return fibo(n - 1) + fibo(n - 2);
+    if(n <= 1) {
+        return n;
+    }
+    return fibo(n-1) + fibo(n-2);
 }
 
-int main()
+static void Fibo(benchmark::State& state)
 {
-    auto memfibo = mem::memoize(fibo); 
-    std::cout << memfibo(40) << std::endl;  
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(fibo(state.range(0)));
+    }
 }
+
+static void FiboMemoize(benchmark::State& state)
+{
+    auto memfibo = mem::memoize(fibo);
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(memfibo(state.range(0)));
+    }
+}
+
+BENCHMARK(Fibo)->Arg(8)->Arg(16)->Arg(24)->Arg(32)->Arg(40);
+BENCHMARK(FiboMemoize)->Arg(8)->Arg(16)->Arg(24)->Arg(32)->Arg(40);
+BENCHMARK_MAIN();
